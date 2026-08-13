41fb06082174aa91a5b5b237db1d0238ce2ccac4
Niraj Joshi <infocodewithniraj@gmail.com>
2026-08-13 14:12:04 +0545

    login fix

--- portfolio/blog-design/src/lib/supabase.js
+++ portfolio/blog-design/src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://tdgmzwjevbyxzgqjqlir.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkZ216d2pldmJ5eHpncWpxbGlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNTcxMDQsImV4cCI6MjEwMTkzMzEwNH0.V2TdM_5RDyTwfq365tazGifMPPZ08LHxSBLIyJ4QhBs";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
